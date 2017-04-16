8926b0d8845a63b7e9bd04ab0d1641e4868ff518
Greg Venech <greg.venech@gmail.com>
2017-04-16 18:26:59 -0400

    refactor: re-pass the required name prop to render list

--- src/app.jsx
+++ src/app.jsx
@@ -3,26 +3,15 @@ import ReactDOM from 'react-dom';
 import DevContainer from 'Components/dev-container/dev-container';
 import Wrapper from 'Components/wrapper/wrapper';
 
+// TODO: Re-evaluate, why exactly are we passing it this way?
+// Can we just always keep it in a working state, maybe just disabling or imitating voting?
+// export default ({ section, page }) => {
+// let arr = page.url.split('/');
+// let name = arr[arr.length - 1];
+// name={ name === 'vote' ? 'todo' : name }
+
 ReactDOM.render((
     <DevContainer>
-        <Wrapper />
+        <Wrapper name="todo" />
     </DevContainer>
 ), document.getElementById('root'));
-
-// export default ({ section, page }) => {
-//   let arr = page.url.split('/');
-//   let name = arr[arr.length - 1];
-
-//   return (
-//     <Container className="vote-list">
-
-//       <section className="vote-list__content">
-//         <Interactive
-//           id="components/vote/app.jsx"
-//           component={ VoteApp }
-//           name={ name === 'vote' ? 'todo' : name }
-//         />
-//       </section>
-//     </Container>
-//   );
-// };
