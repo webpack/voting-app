96a35354e8975dcb5991414c0045bbadc49cd88a
Greg Venech <greg.venech@gmail.com>
2017-04-22 23:44:32 -0400

    refactor(config): finish `dist` configuration
    Extract/minimize CSS and enter the actual top-level app component instead
    of our development environment.

--- webpack.dist.babel.js
+++ webpack.dist.babel.js
@@ -5,6 +5,10 @@ import Merge from 'webpack-merge'
 import CommonConfig from './webpack.common.babel.js'
 
 export default env => Merge(CommonConfig(env), {
+    entry: {
+        vote: './components/wrapper/wrapper.jsx'
+    },
+
     plugins: [
         new Webpack.optimize.UglifyJsPlugin({
             comments: false
