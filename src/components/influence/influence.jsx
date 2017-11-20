87bef003dfb45bc822cc64624caf71a449c29fcd
Greg Venech <greg.venech@gmail.com>
2017-11-20 18:57:45 -0500

    refactor(influence): update the influence description design

--- src/components/influence/influence.jsx
+++ src/components/influence/influence.jsx
@@ -3,14 +3,27 @@ import './influence-style';
 
 export default props => {
     return props.type === 'normal' ? (
-        <section className="influence-info influence-info__section">
-            <h1 className="influence-info__header">Influence</h1>
-            <p><em>Influence</em> is a unit of measure based on time you have been a member on GitHub. However, in 2017 and on you will recieve one influence per day.</p>
+        <section className="influence">
+            <h2 className="influence__header">
+                Influence
+            </h2>
+            <p>
+                <em>Influence</em> is a unit of measure based on time you have been a member on GitHub. However, 
+                from 2017 on you will recieve one influence per day.
+            </p>
         </section>
     ) : (
-        <section className="influence-info influence-info__section">
-            <h1 className="influence-info__header">Golden Influence</h1>
-            <p><em>Golden Influence</em> is equal to 100 <i>normal influence</i>. Golden Influence is obtained by being a backer or sponsor on our <a href="https://opencollective.com/webpack">Open Collective page</a>.</p>
+        <section className="influence">
+            <h2 className="influence__header">
+                Golden Influence
+            </h2>
+            <p>
+                <em>Golden Influence</em> is equal to 100 <i>normal influence</i>. Golden Influence is obtained 
+                by being a backer or sponsor on our 
+                <a href="https://opencollective.com/webpack">
+                    Open Collective page
+                </a>.
+            </p>
         </section>
     );
 };
