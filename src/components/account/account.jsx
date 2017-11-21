324b98eaab416782e79d5b2225f70d7a0ddf785a
Greg Venech <greg.venech@gmail.com>
2017-11-20 19:05:01 -0500

    refactor(account): redesign the account/login component
    Split out the currency icon into a separate component for use elsewhere. Update
    the login button design. Update the display of avatar, available influence, options,
    username, and display name.

--- src/components/account/account.jsx
+++ src/components/account/account.jsx
@@ -1,90 +1,94 @@
 import React from 'react';
-import { startLogin as StartLogin } from 'Utils/js/api';
+import CurrencyIcon from 'Components/currency-icon/currency-icon';
 import GithubLogo from './github-logo.svg';
+import { startLogin as StartLogin } from 'Utils/js/api';
 import './account-style';
 
 // Specify BEM block name
 const block = 'account';
 
 export default class Account extends React.Component {
-    render() {
-        let { userData, loading } = this.props;
+    constructor(props) {
+        super(props);
 
-        if ( !userData ) {
-            if ( loading ) {
-                return (
-                    <div className={ `${block}__inner` }>
-                        Loading user info...
-                    </div>
-                );
-            }
-            
-            return (
-                <div className={ `${block}__login` }>
-                    <button onClick={ this._login.bind(this) }>
-                        Login with Github&nbsp;
+        this.state = {
+            options: false
+        };
+    }
+
+    render() {
+        let { userData, possibleVotes = [], loading } = this.props,
+            { currencies = [] } = userData || {};
+
+        return (
+            <div className={ block }>
+                { !userData && loading ? (
+                    <span>Loading user info...</span>
+                    
+                ) : !userData ? (
+                    <button
+                        className={ `${block}__login` }
+                        onClick={ this._login.bind(this) }>
+                        Login with GitHub&nbsp;
                         <img src={ GithubLogo } />
                     </button>
-                </div>
-            );
 
-        } else {
-            return (
-                <div className={ `${block}__inner` }>
-                    <div className={ `${block}__info` }>
-                        <img alt={ userData.login } src={ userData.avatar } />
-                        { userData.login }
+                ) : (
+                    <div className={ `${block}__inner` }>
+                        <div className={ `${block}__info` }>
+                            <div className={ `${block}__title` }>
+                                { userData.name }
+                                <span className={ `${block}__separator` }>//</span>
+                                { userData.login }
+                            </div>
+                            <div className={ `${block}__currencies` }>
+                                {
+                                    possibleVotes
+                                        .map(settings => currencies.find(obj => obj.name === settings.currency))
+                                        .map(({ name, displayName, remaining, used, value }) => (
+                                            <span
+                                                key={ name }
+                                                className={ `${block}__currency` }
+                                                title={ `You used ${used} of a total of ${value} ${displayName}.` }>
+                                                { remaining }
+                                                <CurrencyIcon type={ name } />
+                                            </span>
+                                        ))
+                                }
+                            </div>
+                        </div>
+
+                        <img 
+                            className={ `${block}__avatar` }
+                            alt={ userData.login }
+                            src={ userData.avatar }
+                            onClick={ this._toggleOptions.bind(this) } />
+
+                        { this.state.options ? (
+                            <div className={ `${block}__options` }>
+                                <button
+                                    className={ `${block}__option` }
+                                    onClick={ this.props.refresh }>
+                                    Refresh
+                                </button>
+                                <button
+                                    className={ `${block}__option` }
+                                    onClick={ this._logout.bind(this) }>
+                                    Logout
+                                </button>
+                            </div>
+                        ) : null }
                     </div>
-
-                    { userData && (
-                        <ul className={ `${block}__currencies` }>
-                            { this._currencies }
-                        </ul> 
-                    )}
-
-                    <div className={ `${block}__options` }>
-                        <button onClick={ this.props.refresh }>
-                            Refresh
-                        </button>
-                        <button onClick={ this._logout.bind(this) }>
-                            Logout
-                        </button>
-                    </div>
-                </div>
-            );
-        }
+                )}
+            </div>
+        );
     }
 
     /**
-     * Amount of influence available to the current user
+     * Initiate GitHub login process
      * 
-     * @return {array} - List of markup
      */
-    get _currencies() {
-        let { userData = {}, possibleVotes = [] } = this.props,
-            { currencies = [] } = userData;
-
-        return currencies.filter(currency => {
-                return possibleVotes.some(voteSettings => {
-                    return voteSettings.currency === currency.name;
-                });
-            })
-            .map(currency => (
-                <li 
-                    key={ currency.name } 
-                    className={ `${block}__currency-${currency.name}` } 
-                    title={ `${currency.description}\nYou used ${currency.used} of a total of ${currency.value} ${currency.displayName}.` }>
-                    { currency.remaining } { currency.displayName }
-                </li>
-            )); 
-    }
-
-    /**
-     * Initiate github login process
-     * 
-     * @param {object} e - React synthetic event
-     */
-    _login(e) {
+    _login() {
         let { location = '' } = window;
 
         StartLogin(location);
@@ -93,10 +97,19 @@ export default class Account extends React.Component {
     /**
      * Log the user out by removing their token
      * 
-     * @param {object} e - React synthetic event
      */
-    _logout(e) {
+    _logout() {
         delete window.localStorage.voteAppToken;
         window.location.reload();
+    }
+
+    /**
+     * Display and hide the options menu
+     * 
+     */
+    _toggleOptions() {
+        this.setState({
+            options: !this.state.options
+        });
     }
 }
