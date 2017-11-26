4b47306ec2f74ce7e70dca28703115c0c3531dd5
Greg Venech <greg.venech@gmail.com>
2017-11-26 00:12:35 -0500

    refactor: remove unnecessary todo

--- src/app.jsx
+++ src/app.jsx
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import DevContainer from 'Components/dev-container/dev-container';
 import Wrapper from 'Components/wrapper/wrapper';
 
-// TODO: Re-evaluate, why exactly are we passing it this way?
-// Can we just always keep it in a working state, maybe just disabling or imitating voting?
-// export default ({ section, page }) => {
-// let arr = page.url.split('/');
-// let name = arr[arr.length - 1];
-// name={ name === 'vote' ? 'todo' : name }
-
 ReactDOM.render((
     <DevContainer>
         <Wrapper name="todo" />
