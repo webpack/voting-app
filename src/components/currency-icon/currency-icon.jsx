1e41372f5fcd58c04cbf95e7010814da4f28bc08
Greg Venech <greg.venech@gmail.com>
2017-11-26 16:11:58 -0500

    chore(currency-icon): use an svg that mimics the logo icon
    This icon now resembles the same general hexagonal shape that the logo
    uses. This commit tweaks how the props/classes are applied so they work
    with the new svg.

--- src/components/currency-icon/currency-icon.jsx
+++ src/components/currency-icon/currency-icon.jsx
@@ -4,10 +4,25 @@ import './currency-icon-style';
 // Specify BEM block name
 const block = 'currency-icon';
 
+// ...
+const sizes = {
+    small: 10,
+    large: 16,
+    huge: 19
+};
+
 export default ({
     type,
     size = 'small',
     ...props
 }) => (
-    <span className={ `${block} ${block}--${type} ${block}--${size}` } />
+    <svg
+        className={ block }
+        width={ sizes[size] }
+        height={ sizes[size] }
+        viewBox="0 0 78 78">
+        <g className={ `${block}--${type}` } >
+            <polygon points="5 19.6258065 38.4980127 0 73 19.6258065 73 57.3677419 38.5 78 5 57.3677419" />
+        </g>
+    </svg>
 );
