4060504364ea7e70b5c309dcd32aad2a13957faf
Greg Venech <greg.venech@gmail.com>
2017-11-26 00:12:12 -0500

    fix(config): fix webpack configuration to properly export the top-level component

--- webpack.dist.babel.js
+++ webpack.dist.babel.js
@@ -14,7 +14,17 @@ export default env => Merge(CommonConfig(env), {
     ],
 
     externals: {
-        react: 'react',
-        'react-dom': 'react-dom'
+        react: {
+            root: "React",
+            commonjs2: "react",
+            commonjs: "react",
+            amd: "react"
+        },
+        'react-dom': {
+            root: "ReactDOM",
+            commonjs2: "react-dom",
+            commonjs: "react-dom",
+            amd: "react-dom"
+        }
     }
 })
