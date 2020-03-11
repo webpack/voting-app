2ae087f9ef3c632bca7f08cc65f1606b3563ac87
Eugene Hlushko <jhlushko@gmail.com>
2020-03-11 21:29:24 +0200

    chore(deps) upgrade deps and webpack to 4 (#20)
    * chore(deps) upgrade deps and webpack to 4
    
    * chore(deps) update more deps, remove unused after migration

--- webpack.dist.babel.js
+++ webpack.dist.babel.js
@@ -1,30 +1,30 @@
-import Path from 'path'
-import Webpack from 'webpack'
 import Merge from 'webpack-merge'
+import cssnano from 'cssnano'
+import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin'
 
 import CommonConfig from './webpack.common.babel.js'
 
 export default env => Merge(CommonConfig(env), {
     entry: './components/wrapper/wrapper.jsx',
 
-    plugins: [
-        new Webpack.optimize.UglifyJsPlugin({
-            comments: false
-        })
-    ],
-
     externals: {
         react: {
-            root: "React",
-            commonjs2: "react",
-            commonjs: "react",
-            amd: "react"
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
         },
         'react-dom': {
-            root: "ReactDOM",
-            commonjs2: "react-dom",
-            commonjs: "react-dom",
-            amd: "react-dom"
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom'
         }
-    }
+    },
+    plugins: [
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.min\.css$/g,
+            cssProcessor: cssnano
+        })
+    ]
 })
