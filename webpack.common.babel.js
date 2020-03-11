0cb9aaec0c753f5c13c8cc3ea100a2b05042c85d
EugeneHlushko <jhlushko@gmail.com>
2020-03-11 21:16:31 +0200

    chore(deps) upgrade deps and webpack to 4

--- webpack.common.babel.js
+++ webpack.common.babel.js
@@ -1,13 +1,14 @@
 import Path from 'path'
-import Webpack from 'webpack'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 export default (env = {}) => ({
     context: Path.resolve(__dirname, './src'),
 
+    mode: env.dev ? 'development' : 'production',
+
     resolve: {
         symlinks: false,
-        extensions: [ '.js', '.jsx', '.scss' ],
+        extensions: ['.js', '.jsx', '.scss'],
         alias: {
             Components: Path.resolve(__dirname, './src/components'),
             Utils: Path.resolve(__dirname, './src/utils')
@@ -20,7 +21,7 @@ export default (env = {}) => ({
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: [
-                    'babel-loader', 
+                    'babel-loader',
                     {
                         loader: 'eslint-loader',
                         options: {
@@ -32,44 +33,34 @@ export default (env = {}) => ({
             },
             {
                 test: /\.s?css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                minimize: !env.dev
-                            }
-                        },
-                        'postcss-loader', 
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                includePaths: [
-                                    Path.resolve(__dirname, './src/utils/scss') 
-                                ]
-                            }
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [
+                                require('autoprefixer')
+                            ],
                         }
-                    ]
-                })
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: [Path.resolve(__dirname, './src/utils/scss')]
+                        }
+                    }
+                ]
             },
             {
                 test: /\.(jpg|png|svg)$/,
-                use: [
-                    'file-loader',
-                    'image-webpack-loader'
-                ]
+                use: 'file-loader'
             }
         ]
     },
 
     plugins: [
-        new Webpack.DefinePlugin({
-            'process.env.NODE_ENV': env.dev ? `'development'` : `'production'` 
-        }),
-
-        new ExtractTextPlugin({
-            disable: !!env.dev,
+        new MiniCssExtractPlugin({
             filename: 'style.min.css'
         })
     ],
--- webpack.dist.babel.js
+++ webpack.dist.babel.js
@@ -1,5 +1,3 @@
-import Path from 'path'
-import Webpack from 'webpack'
 import Merge from 'webpack-merge'
 
 import CommonConfig from './webpack.common.babel.js'
@@ -7,24 +5,18 @@ import CommonConfig from './webpack.common.babel.js'
 export default env => Merge(CommonConfig(env), {
     entry: './components/wrapper/wrapper.jsx',
 
-    plugins: [
-        new Webpack.optimize.UglifyJsPlugin({
-            comments: false
-        })
-    ],
-
     externals: {
         react: {
-            root: "React",
-            commonjs2: "react",
-            commonjs: "react",
-            amd: "react"
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
         },
         'react-dom': {
-            root: "ReactDOM",
-            commonjs2: "react-dom",
-            commonjs: "react-dom",
-            amd: "react-dom"
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom'
         }
     }
 })
